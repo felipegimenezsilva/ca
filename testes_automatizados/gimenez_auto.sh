e236f7d94931bce78b8e8afee69d9c77b60bcddb
Felipe Gimenez da Silva <gimenez@Gimenez.gimenez>
2021-02-08 11:00:05 -0300

    preparaçao para test pthread

--- testes_automatizados/gimenez_auto.sh
+++ testes_automatizados/gimenez_auto.sh
@@ -2,10 +2,10 @@
 
 result_folder="result"
 build_folder="build"
-threads_number=(1 2 3 4)
+threads_number=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
 
 # random_spheres,two_spheres,perlin_spheres,earth,light_sample,cornell_box,instance_test,cornell_smoke,showcase,metal_test
-flags=("-s "{10,30}" --scene "{random_spheres,two_spheres})
+flags=("-s 1000 --scene "{random_spheres,two_spheres,perlin_spheres,earth,light_sample,cornell_box,instance_test,cornell_smoke,showcase,metal_test})
 
 function bar
 {
